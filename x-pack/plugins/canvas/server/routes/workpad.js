b2c2dc986a650ef6b35f7ee549ebe437ebbbc259
Joe Fleming <w33ble@users.noreply.github.com>
2019-04-19 14:43:20 -0700

    Update x-pack/plugins/canvas/server/routes/workpad.js
    Co-Authored-By: cqliu1 <catherineqliu@outlook.com>

--- x-pack/plugins/canvas/server/routes/workpad.js
+++ x-pack/plugins/canvas/server/routes/workpad.js
@@ -16,7 +16,6 @@ import { getId } from '../../public/lib/get_id';
 import { formatResponse as formatRes } from '../lib/format_response';
 
 export function workpad(server) {
-  //const config = server.config();
   const { errors: esErrors } = server.plugins.elasticsearch.getCluster('data');
   const routePrefix = API_ROUTE_WORKPAD;
   const routePrefixAssets = API_ROUTE_WORKPAD_ASSETS;
